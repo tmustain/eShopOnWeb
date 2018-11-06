5951c357d1c3b6c22480bb4e404f9e7c3fe17f04
Tim Mustain <timothy.mustain@gmail.com>
2018-11-06 01:16:16 -0800

    Update Startup.cs

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -100,9 +100,6 @@ namespace Microsoft.eShopWeb.Web
 
             // Add memory cache services
             services.AddMemoryCache();
-            
-            services.Configure<IISOptions>(options => options.ForwardWindowsAuthentication = true);
-
             services.AddMvc()
                 .SetCompatibilityVersion(AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
 
