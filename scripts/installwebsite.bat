6eb667ef87eb7dae6857a0bf75e1517ccd97b9db
Tim Mustain <timothy.mustain@bydeluxe.com>
2018-11-12 22:19:50 -0800

    added logs dir

--- scripts/installwebsite.bat
+++ scripts/installwebsite.bat
@@ -4,6 +4,8 @@ IF NOT EXIST c:\inetpub\wwwroot\eshop mkdir c:\inetpub\wwwroot\eshop
 
 IF NOT EXIST c:\inetpub\wwwroot\eshop\bin mkdir c:\inetpub\wwwroot\eshop\bin
 
+IF NOT EXIST c:\inetpub\wwwroot\eshop\logs mkdir c:\inetpub\wwwroot\eshop\logs
+
 cd c:\temp
 
 %SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command ".\installwebsite.ps1"
